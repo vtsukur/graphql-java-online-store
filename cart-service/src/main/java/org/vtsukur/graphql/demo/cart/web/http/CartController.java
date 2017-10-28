8dfa7ff53f48727ce85d7dcbd1fa31e7347667c5
vtsukur <volodymyrt@wix.com>
2017-10-28 19:55:26 +0300

    HTTP API: projection of Cart with expanded Product data

--- cart-service/src/main/java/org/vtsukur/graphql/demo/cart/web/http/CartController.java
+++ cart-service/src/main/java/org/vtsukur/graphql/demo/cart/web/http/CartController.java
@@ -1,28 +1,67 @@
 package org.vtsukur.graphql.demo.cart.web.http;
 
+import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.vtsukur.graphql.demo.cart.deps.ProductServiceRestClient;
 import org.vtsukur.graphql.demo.cart.domain.Cart;
+import org.vtsukur.graphql.demo.cart.domain.CartItem;
 import org.vtsukur.graphql.demo.cart.domain.CartService;
+import org.vtsukur.graphql.demo.product.api.Product;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/carts")
 public class CartController {
 
     private final CartService cartService;
 
+    private final ProductServiceRestClient productServiceRestClient;
+
     @Autowired
-    public CartController(CartService cartService) {
+    public CartController(CartService cartService, ProductServiceRestClient productServiceRestClient) {
         this.cartService = cartService;
+        this.productServiceRestClient = productServiceRestClient;
     }
 
-    @RequestMapping("/{id}")
+    @RequestMapping("/carts/{id}")
     @ResponseBody
-    public Cart get(@PathVariable Long id) {
-        return cartService.findCart(id);
+    public Object get(@PathVariable Long id,
+                      @RequestParam(value = "projection", required = false) String projection) {
+        val cart = cartService.findCart(id);
+        if ("with-products".equals(projection)) {
+            return getProjectionWithProducts(cart);
+        }
+        return cart;
+    }
+
+    private CartWithProductsProjection getProjectionWithProducts(Cart source) {
+        val cart = new CartWithProductsProjection();
+        cart.setId(source.getId());
+        cart.setItems(toItemsWithProducts(source.getItems()));
+        cart.setSubTotal(source.getSubTotal());
+        return cart;
+    }
+
+    private List<CartWithProductsProjection.Item> toItemsWithProducts(List<CartItem> source) {
+        return source.stream().map(sourceItem -> {
+            val item = new CartWithProductsProjection.Item();
+            item.setProduct(toProductProjection(productServiceRestClient.fetchProduct(sourceItem.getProductId())));
+            item.setQuantity(sourceItem.getQuantity());
+            item.setTotal(sourceItem.getTotal());
+            return item;
+        }).collect(Collectors.toList());
+    }
+
+    private CartWithProductsProjection.Item.ProductProjection toProductProjection(Product source) {
+        val product = new CartWithProductsProjection.Item.ProductProjection();
+        product.setId(source.getId());
+        product.setTitle(source.getTitle());
+        product.setPrice(source.getPrice());
+        product.setSku(source.getSku());
+        product.setImage(source.getImages().get(0));
+        return product;
     }
 
 }
