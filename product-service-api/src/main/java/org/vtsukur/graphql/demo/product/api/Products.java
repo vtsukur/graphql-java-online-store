a5edcd17832b5bb875d6818b2afe03a88197e3ab
vtsukur <volodymyrt@wix.com>
2018-02-18 18:38:08 +0200

    Adding no-argument constructor to fix Jackson serialization issues

--- product-service-api/src/main/java/org/vtsukur/graphql/demo/product/api/Products.java
+++ product-service-api/src/main/java/org/vtsukur/graphql/demo/product/api/Products.java
@@ -2,10 +2,12 @@ package org.vtsukur.graphql.demo.product.api;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class Products {
 
